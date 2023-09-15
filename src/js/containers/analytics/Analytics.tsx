c10248d0b2153534ff44e1b49da9faeea7656115
adithya-keyvalue <adithya.v@keyvalue.systems>
2023-09-15 13:12:37 +0530

    Integrate member metrics feature

--- src/js/containers/analytics/Analytics.tsx
+++ src/js/containers/analytics/Analytics.tsx
@@ -1,5 +1,32 @@
-import React from "react";
+import React, { useState } from "react";
+import "chart.js/auto";
+import MemberMetrics from "../../components/Analytics/MemberMetrics";
 
-const Analytics = () => <div className="bg-red-300">Analytics</div>;
+const Analytics = () => {
+  const [selectedRange, setSelectedRange] = useState("WEEKLY");
+
+  return (
+    <div className="pl-8 pt-12 pr-[72px]">
+      <div className="flex justify-between">
+        <div className="ml-4 text-2xl text-white">
+          Good Morning{" "}
+          <span className="font-bold text-[#15BFFD] text-3xl">John Doe</span>
+        </div>
+        <div className="flex gap-10">
+          <img src="icons/help-icon.svg" alt="help" />
+          <img src="icons/alert-icon.svg" alt="alert" />
+          <img src="icons/profile-dp.svg" alt="profile" />
+        </div>
+      </div>
+      <div className="flex justify-between pt-11">
+        <MemberMetrics
+          selectedRange={selectedRange}
+          setSelectedRange={setSelectedRange}
+        />
+        <div className="w-2/5 bg-[#222338] rounded-xl">kjhvv</div>
+      </div>
+    </div>
+  );
+};
 
 export default Analytics;
